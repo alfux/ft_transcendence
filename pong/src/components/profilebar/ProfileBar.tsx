b1330c791319a609854588b8fac317725d03bad6
Rémi Claire <reclaire@student.42mulhouse.fr>
2023-12-05 17:51:06 +0100

    fix profile

--- pong/src/components/profilebar/ProfileBar.tsx
+++ pong/src/components/profilebar/ProfileBar.tsx
@@ -3,21 +3,26 @@ import Cookies from 'js-cookie';
 import React, { useRef, useEffect, useState } from 'react'
 import usePayload from '../../react_hooks/use_auth'
 import userEvent from '@testing-library/user-event';
+import { LoggedStatus } from '../../THREE/Utils/jwt.interface';
+
 const accessToken = Cookies.get('accessToken')
+
 type User = {
-  avatar: string;
+  id: number;
   email: string;
-  firstName: string;
-  lastName: string;
-  nickName: string;
+  image: string;
+  isAuthenticated: LoggedStatus;
+  twoFactorAuth: boolean;
+  username: string;
 }
+
 const ProfileBar: React.FC = () => {
   const [payload, updatePayload, handleUpdate] = usePayload();
   const [data, setData] = useState<User | null>(null)
   useEffect(() => {
     const requestProfile = async () => {
       try {//fetch Profile
-        const enable2FAEndpoint = 'http://localhost:3001/api/user/me' + payload?.sub;
+        const enable2FAEndpoint = 'http://localhost:3001/api/user/me';
         console.log('Before fetch');
         const response = await fetch(enable2FAEndpoint, {
           method: 'GET',
@@ -41,18 +46,18 @@ const ProfileBar: React.FC = () => {
   return (
     <div className="profile-bar">
       {data != null ? (
-        <img className="profile-photo" src={data.avatar}></img>
+        <img className="profile-photo" src={data.image}></img>
       ) : <h2>nop</h2>}
       {data != null ? (
         <div className='stats'>
-          <p>Nickname</p>
+          <p>Login</p>
           <p>Rank</p>
           <p>Lvl</p>
         </div>
       ) : <h2>nop</h2>}
       {data != null ? (
         <div className='stats-values'>
-          <p>{data.nickName}</p>
+          <p>{data.username}</p>
           <p>1</p>
           <p>10</p>
         </div>
