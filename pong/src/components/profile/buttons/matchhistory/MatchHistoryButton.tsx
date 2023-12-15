9de04a80ba7ca51b5ca0322176d12ee8e8455b8a
Rémi Claire <reclaire@student.42mulhouse.fr>
2023-12-15 12:57:26 +0100

    ahah ca marche

--- pong/src/components/profile/buttons/matchhistory/MatchHistoryButton.tsx
+++ pong/src/components/profile/buttons/matchhistory/MatchHistoryButton.tsx
@@ -1,14 +1,21 @@
 import React, { useRef, useEffect, useState } from 'react'
 import './MatchHistoryButton.css'
+import { Match } from '../../../../THREE/Utils/backend_types';
 
 
-const MatchHistory: React.FC = () => {
-  
+function MatchHistory(props: {
+  matches: Match[]
+}) {
   return (
-    <div >
-        <button className='glowing-btn2'><span className='glowing-txt2'>Match<span className='faulty-letter2'> History</span></span></button>
+    <div style={{width:'90%'}}> {/* hacky patch to make sure enough place for text */}
+      <button className='glowing-btn2'>
+        <span className='glowing-txt2'>Match
+          <span className='faulty-letter2'> History</span>
+        </span>
+      </button>
     </div>
   );
-};
+
+}
 
 export default MatchHistory;
