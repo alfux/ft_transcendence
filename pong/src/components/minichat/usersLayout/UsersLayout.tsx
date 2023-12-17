dd633e7264f06acd20a2ded4820a9ac37c820821
Rémi Claire <reclaire@student.42mulhouse.fr>
2023-12-17 18:18:14 +0100

    fix

--- pong/src/components/minichat/usersLayout/UsersLayout.tsx
+++ pong/src/components/minichat/usersLayout/UsersLayout.tsx
@@ -3,15 +3,16 @@ import { LoggedStatus } from "../../../THREE/Utils/jwt.interface";
 import { ChannelOptions, ChatProps } from "../MiniChat";
 
 const UsersLayout: React.FC<ChatProps> = (props) => {
+
   const onlineUsers = props.allUsers?.map((user: User) => {
-    return user.isAuthenticated === LoggedStatus.Logged && user.id !== props.me?.id ? (
+    return user.id !== props.me?.id ? (
       <img
         key={user.id}
         src={user.image}
         className={user?.isAuthenticated === 0 ? "chat-user-online" : "chat-user-offline"}
         onClick={() => props.setSelectedUser(user)}
       />
-    )
+    ) : undefined
   });
 
   const myFriends = props.friends?.map((friend: User) => {
